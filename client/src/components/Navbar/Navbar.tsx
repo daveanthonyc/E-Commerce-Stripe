3f4db495725d9c0212c81496c3623028aeb6536e
daveanthonyc <daveanthonyc@gmail.com>
2024-06-15 19:29:57 +1000

    First commit

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="wrapper">
+                <AddCircleOutlineIcon />
+                <div className="left"></div>
+                <div className="center"></div>
+                <div className="right"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ client/src/main.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.tsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
+import Product from './pages/Product/Product';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+const Layout = () => {
+    return (
+        <div className='app'>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+        {
+            path: '/',
+            element: <Home />
+        },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
